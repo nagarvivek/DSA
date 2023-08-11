170a87355ae6bfe4263c6e53040e1949a8934adc
Vivek Nagar <92619450+nagarvivek@users.noreply.github.com>
2023-08-11 12:00:33 +0530

    Added solution - LeetHub

--- Check for BST - GFG/check-for-bst.cpp
+++ Check for BST - GFG/check-for-bst.cpp
@@ -22,28 +22,14 @@ struct Node {
 class Solution
 {
     public:
-    //Function to check whether a Binary Tree is BST or not.
-    void inorder(Node* root, vector<int>& ans) {
-        if (!root) return;
-        inorder(root->left, ans);
-        ans.push_back(root->data);
-        inorder(root->right, ans);
-    }
-
-    bool isBST(Node* root) {
-        vector<int> ans;
-        inorder(root, ans);
-
-        for (int i = 1; i < ans.size(); i++) {  
-            if (ans[i] <= ans[i - 1])  
-                return false;
-        }
-
-        return true;
+    
+    bool isBST(Node* root, int left = INT_MIN, int right = INT_MAX) 
+    {
+        if(root==NULL) return true;
+        if(root->data<=left || root->data>=right) return false;
+        return isBST(root->left, left, root->data) && isBST(root->right, root->data, right);
     }
 };
-
-
 
 
 //{ Driver Code Starts.
