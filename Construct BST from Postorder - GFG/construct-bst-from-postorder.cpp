47c2f1a5a9153a5b3cda1435f3eccd5adf613306
Vivek Nagar <92619450+nagarvivek@users.noreply.github.com>
2023-08-13 10:16:27 +0530

    Added solution - LeetHub

---
+++ Construct BST from Postorder - GFG/construct-bst-from-postorder.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+Node *constructTree (int [], int );
+
+void printInorder (Node* node)
+{
+	if (node == NULL)
+		return;
+	printInorder(node->left);
+	printf("%d ", node->data);
+	printInorder(node->right);
+	
+
+}
+
+int main ()
+{   
+    int t,n;
+   scanf("%d",&t);
+    while(t--){
+        scanf("%d",&n);
+        int post[n];
+        for(int i=0;i<n;i++)
+        scanf("%d",&post[i]);
+
+    Node* root = constructTree(post, n);
+
+	printInorder(root);
+    printf("\n");
+}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*struct Node
+{
+	int data;
+	Node *left, *right;
+};*/
+Node* create(int post[], int left, int right, int& i) {
+    if (i < 0 || post[i] > right || post[i] < left) {
+        return nullptr;
+    }
+
+    Node* root = new Node(post[i]);
+    i--;
+
+    root->right = create(post, root->data + 1, right, i);
+    root->left = create(post, left, root->data - 1, i);
+
+    return root;
+}
+
+Node* constructTree(int post[], int size) {
+    int i = size - 1;
+
+    return create(post, INT_MIN, INT_MAX, i);
+}
